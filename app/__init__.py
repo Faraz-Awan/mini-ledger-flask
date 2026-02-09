9ee320df00c65cad28a0230c19c3f03a5f89b4bb
Faraz-Awan <fa.awan100@gmail.com>
2026-02-09 11:10:14 -0500

    Set up Flask app with Postgres connection and Alembic

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from sqlalchemy import text
+from app.db import engine
+
+with engine.connect() as conn:
+    conn.execute(text("SELECT 1"))
 
 def create_app() -> Flask:
     app = Flask(__name__)
